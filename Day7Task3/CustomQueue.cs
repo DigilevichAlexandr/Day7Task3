d4030d6f726b16a7c6cb0f36805d05c0030cf85e
Alexandr <digilevich.alexandr@gmail.com>
2015-07-30 16:04:50 +0300

    1

---
+++ Day7Task3/CustomQueue.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day7Task3
+{
+    public class CustomQueue<T>
+    {
+        private readonly T[] _queue;
+        int _length;
+        public int Count { get { return _queue.Length; } }
+        public CustomQueue(T[] arr)
+        {
+            _queue = arr;
+            _length = arr.Length;
+        }
+
+        public void Enqueue(T element)
+        {
+            _queue[_length++] = element;
+        }
+
+        public T Denqueue()
+        {
+            if (_length > 0)
+            {
+                var result = _queue[0];
+                for (int i = 0; i < _length; )
+                    _queue[i] = _queue[++i];
+                return result;
+            }
+            else
+                return default(T);
+        }
+
+        public T Peek()
+        {
+            if (_length > 0)
+            {
+                return _queue[0];
+            }
+            else
+                return default(T);
+        }
+
+        private T PeekInd(int ind)
+        {
+            return _queue[ind];
+        }
+        
+        public CustomIterator Iterator()
+        {
+            return new CustomIterator(this);
+        }
+
+        public class CustomIterator
+        {
+            private readonly CustomQueue<T> collection;
+            private int currentIndex;
+
+            internal CustomIterator(CustomQueue<T> collection)
+            {
+                this.currentIndex = -1;
+                this.collection = collection;
+            }
+
+            public T Current
+            {
+                get
+                {
+                    if (currentIndex == -1 || currentIndex == collection.Count)
+                    {
+                        throw new InvalidOperationException();
+                    }
+                    return collection.PeekInd(currentIndex);
+                }
+            }
+
+            public void Reset()
+            {
+                currentIndex = -1;
+            }
+
+            public bool MoveNext()
+            {
+                return ++currentIndex < collection.Count;
+            }
+        }
+    }
+}
---
+++ Day7Task3/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day7Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var queue = new CustomQueue<string>(new[]
+            {
+                "one", "two", "three", "four"
+            }
+            );
+
+            CustomQueue<string>.CustomIterator iterator = queue.Iterator();
+
+            while (iterator.MoveNext())
+            {
+                Console.WriteLine(iterator.Current);
+            }
+            iterator.Reset();
+            while (iterator.MoveNext())
+            {
+                Console.WriteLine(iterator.Current);
+            }
+            var cueueInt = new CustomQueue<int>(new[]
+            {
+                1, 2, 3, 4
+             });
+
+            CustomQueue<int>.CustomIterator iterator1 = cueueInt.Iterator();
+
+            while (iterator1.MoveNext())
+            {
+                Console.WriteLine(iterator1.Current);
+            }
+            iterator1.Reset();
+            Console.ReadLine();
+        }
+        
+    }
+}
